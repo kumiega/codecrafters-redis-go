020ab238a5182a69456b4e8cf900e5db05546bab
Tomasz Kumięga <poczta@kumiega.org>
2024-05-15 21:58:41 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -12,9 +13,47 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+	fmt.Println("Redis server starting on port 6379")
+
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New connection established")
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading from connection:", err)
+			}
+			return
+		}
+
+		ping := "*1\r\n$4\r\nPING\r\n"
+		pong := "+PONG\r\n"
+
+		if ping == string(buf[:n]) {
+			_, err = conn.Write([]byte(pong))
+		}
+
+		if err != nil {
+			fmt.Println("Error writing to connection:", err)
+			return
+		}
 	}
 }
